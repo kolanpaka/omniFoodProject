78d86b101628e64703846ef6b86fe214277bf5db
kolanpaka kalyan <kolanpaka2000@gmail.com>
2024-03-11 00:08:16 +0530

    scrolling Behaviour is fixed

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ let menubar = document.getElementById("menubar");
 let navUl = document.querySelector("nav ul");
 let headerContainer = document.getElementById("headerContainer");
 
-
 menubar.addEventListener("click", function () {
   let isEvenClick = menubar.classList.toggle("fa-xmark");
+  if (isEvenClick) {
+    document.body.classList.add("no-scroll");
+  } else {
+    document.body.classList.remove("no-scroll");
+  }
   menubar.classList.toggle("fa-bars");
   navUl.classList.toggle("animateSlideToLeft", isEvenClick);
   navUl.classList.toggle("animateSlideToRight", !isEvenClick);
 });
 
-
-function removeSlide(){
+function removeSlide() {
   menubar.classList.remove("fa-xmark");
+  document.body.classList.remove("no-scroll");
   menubar.classList.add("fa-bars");
-  navUl.classList.remove("animateSlideToLeft")
+  navUl.classList.remove("animateSlideToLeft");
   navUl.classList.add("animateSlideToRight");
 }
 window.addEventListener("scroll", function () {
@@ -23,11 +27,12 @@ window.addEventListener("scroll", function () {
   let windowWidth = window.innerWidth;
   let isScrollUp = scrolled >= 730;
   console.log(windowWidth);
-  if(windowWidth<=980){
-    isScrollUp=true;
-    if(scrolled===0){
-      isScrollUp=false;
+  if (windowWidth <= 980) {
+    isScrollUp = true;
+    if (scrolled === 0) {
+      isScrollUp = false;
     }
   }
   headerContainer.classList.toggle("sticky", isScrollUp);
 });
+
